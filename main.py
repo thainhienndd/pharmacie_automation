e6202e90d46e05e9876f0b3c0d1448f72f49b327
jean1722 <57453485+jean1722@users.noreply.github.com>
2023-09-22 18:26:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def compute_isaac_score(temperature, toux, adenopathie, amygale, age):
 
 
 def realisabilite_trod(isaac_score, gorge):
-    if gorge:
-        if isaac_score >= 2:
+    if  st.session_state['gorge'] == 'Oui':
+        if isaac_score > 1:
             return True
     return False
 
@@ -43,36 +43,37 @@ if 'preliminary_info_validated' not in st.session_state:
 if 'bouton_trod' not in st.session_state:
     st.session_state['bouton_trod'] = False
 
-st.header('Information préliminaire')
-
+st.header('Identification du patient')
 
 st.session_state['name'] = st.text_input('Nom')
 st.session_state['surname'] = st.text_input('Prénom')
-st.session_state['brith_date'] = st.date_input('Date de naissance', datetime.date(2000, 1, 1))
-st.session_state['is_ordonnance'] = st.selectbox("Présence d'une ordonnance", ('Oui', 'Non'))
+st.session_state['brith_date'] = st.date_input('Date de naissance', date.today() ,format="DD/MM/YYYY")
+st.session_state['is_ordonnance'] = st.selectbox("Présentation d'une ordonnance de dispensation conditionnelle (si TROD+) datant au plus de 7 jours", ('Oui', 'Non'))
 st.session_state['age'] = date_actuelle.year - st.session_state['brith_date'].year - ((date_actuelle.month, date_actuelle.day) < (st.session_state['brith_date'].month, st.session_state['brith_date'].day))
 
-if st.button('Valider les informations préliminaires') or st.session_state['preliminary_info_validated']:
+if st.button('Valider') or st.session_state['preliminary_info_validated']:
     st.session_state['preliminary_info_validated'] = True
     if st.session_state['is_ordonnance'] == 'Non':
         st.divider()
-        st.header('Vérification de la réalisabilité du test TROD')
-        st.session_state['temperature'] = st.checkbox('La température du patient >38°')
-        st.session_state['absence_de_toux'] = st.checkbox('Le patient a de la toux')
-        st.session_state['adenopathie'] = st.checkbox('Le patient a une/des adénopathies cervicales douloureuses')
-        st.session_state['amygale'] = st.checkbox('Le patient a une augmentation de volume ou un exsudat amygdalien')
-        st.session_state['gorge'] = st.checkbox("Le patient a l'aspect de la gorge compatible avec une angine érythémateuse ou érythématopultacée")
+        st.header("Critères d'éligibilités du patient")
+        st.subheader("Calcul du score de Mac Isaac")
+        st.caption("Le patient présente les éléments cliniques suivants :")
+        st.session_state['temperature'] = st.checkbox("Température > 38°C")
+        st.session_state['absence_de_toux'] = st.checkbox("Absence de toux")
+        st.session_state['adenopathie'] = st.checkbox("Adénopathie(s) cervicale(s) antérieure(s) douloureuse(s)")
+        st.session_state['amygale'] = st.checkbox("Augmentation de volume ou exsudat amygdalien")
+        st.subheader("Aspect de la gorge")
+        st.session_state['gorge'] = st.selectbox("Le patient a l'aspect de la gorge compatible avec une angine érythémateuse ou érythématopultacée", ('Oui', 'Non'))
         st.session_state['isaac_score'] = compute_isaac_score(st.session_state['temperature'], st.session_state['absence_de_toux'], st.session_state['adenopathie'], st.session_state['amygale'], st.session_state['age'])
         if st.button('Réalisabilité du test TROD') or st.session_state['bouton_trod']:
             st.session_state['bouton_trod'] = True
-            st.text(f'Pour information le score Mac-Isaac est de {st.session_state["isaac_score"]}')
+            st.text(f'Le score Mac-Isaac est de {st.session_state["isaac_score"]}')
             st.session_state['realisabilite_trod'] = realisabilite_trod(st.session_state['isaac_score'], st.session_state['gorge'])
             if st.session_state['realisabilite_trod']:
-                st.text('Test TROD réalisable !')
+                st.text("Le TROD angine est réalisable à l'officine")
                 print_tracabilite_fiche_patient()
             else:
-                st.text('Test TROD non réalisable :(')
+                st.text("Le TROD angine n'est pas réalisable à l'officine car le score de Mac Isaac est inférieur à 2 et/ou l'aspect de la gorge du patient n'est pas compatible.")
     elif st.session_state['is_ordonnance'] == 'Oui':
         print_tracabilite_fiche_patient()
-
 
